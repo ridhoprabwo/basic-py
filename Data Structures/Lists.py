713579df8c4fde56b5777987e26046cc4d4f7462
Ridho Prabowo <ridhoprabwo@gmail.com>
2022-06-23 12:13:35 +0700

    Data Structures

---
+++ Data Structures/Lists.py
@@ -0,0 +1,21 @@
+from math import comb
+
+
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+print(letters, matrix)
+
+zeros = [0] * 5
+print(zeros)
+
+combined = letters + zeros
+print(combined)
+
+numbers = list(range(20))
+print(numbers)
+
+chars = list("Hello World")
+print(chars)
+
+charsLength = len(chars)
+print(charsLength)
